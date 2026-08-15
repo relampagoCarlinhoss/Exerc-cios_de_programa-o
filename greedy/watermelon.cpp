7366ac1b3998edf8a8b227af070f6eda02cf7102
Carlos <carloshlcunha@gmail.com>
2026-08-15 14:38:40 -0300

    warmup__testing codeforces and more

---
+++ greedy/watermelon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+	int peso;
+	cin >> peso;
+
+	if(peso % 2 == 0 && peso != 2){
+		cout << "YES\n";	
+	}
+	
+	else{
+		cout << "NO\n";	
+	}
+
+	return 0; 
+}
+
